bd8d8618c2f21dff194d7c6b28595d84e4c94a5b
akhilendra <akhilendray@umbrellainfocare.com>
2021-03-11 23:46:39 +0530

    ship obj fixed

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,27 +1,26 @@
 import sys
 import pygame
 import game_function as gf
-from settings import settings 
+from settings import settings
 from Ship import ship
+
 def run_game():
- pygame.init()
- ai_settings = settings()
- screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.init()
+    ai_settings = settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien invasion")
+    shipdraw = ship(screen)
 
- pygame.display.set_caption("Alien invasion")  
- shipdraw = ship(screen)
- while True:
-    gf.check_events(shipdraw)
-    gf.update_screen(ai_settings,screen,ship)
-    for event in pygame.event.get():
-         
-     if event.type == pygame.QUIT:
-       sys.exit()
-    screen.fill(ai_settings.bg_color)
-    shipdraw.blitme()
- pygame.display.flip()
- 
+    while True:
+        gf.check_events(shipdraw)
+        gf.update_screen(ai_settings, screen, shipdraw)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.fill(ai_settings.bg_color)
+        shipdraw.blitme()
+
+    pygame.display.flip()
+
+
 run_game()
-
-
-
